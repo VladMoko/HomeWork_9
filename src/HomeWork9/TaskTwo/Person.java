48307e48b214c631abb56f6ed3013f70fcd0685e
VladMoko <advmoney2233@gmail.com>
2022-02-02 22:02:59 +0300

    w

--- src/HomeWork9/TaskTwo/Person.java
+++ src/HomeWork9/TaskTwo/Person.java
@@ -11,21 +11,27 @@ import java.util.Scanner;
 
 public class Person {
     public static void main(String[] args) throws FileNotFoundException {
+        //Считываем данные с файла
         Scanner scanner = new Scanner(new File("file.txt"));
+        //Лист для записи данных
         List<String> list = new ArrayList<>();
-
-        while (scanner.hasNextLine()){
+        //Добавление элементов файла в лист
+        while (scanner.hasNextLine()) {
             list.add(scanner.nextLine());
         }
+        //Удаление лишнего
         list.remove(0);
 
+        //Второй лист
         List<User> users = new ArrayList<>();
-        for (String str : list){
-            String [] array = str.split(" ");
+        //Цикл в котором всё мероприятие под каждого человека ... Alice ...
+        for (String str : list) {
+            String[] array = str.split(" ");
             String name = array[0];
             int age = Integer.parseInt(array[1]);
-            users.add(new User(name,age));
+            users.add(new User(name, age));
 
+            //Перевод в json формат
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
             String json = gson.toJson(users);
             System.out.println(json);
